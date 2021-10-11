5c42c3d84e8537ea2f420d07326fa2a39b6ef2e8
leonardoigor <igor_mendonca@outlook.com.br>
2021-10-11 18:06:48 -0300

    login ng

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AutenticacaoService } from 'src/app/autenticacao/autenticacao.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  user: string = ''
+  password: string = ''
+
+  constructor(private auth: AutenticacaoService) { }
 
   ngOnInit(): void {
+  }
+
+
+  login() {
+    alert(this.user + " " + this.password)
   }
 
 }
