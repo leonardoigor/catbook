8b40c706f744a8144f63a02c48c293fdfe29d877
leonardoigor <igor_mendonca@outlook.com.br>
2021-10-11 13:22:54 -0300

    login

--- src/app/autenticacao/autenticacao.service.ts
+++ src/app/autenticacao/autenticacao.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class AutenticacaoService {
 
   constructor(private httpclient: HttpClient) { }
+
+  autenticar(userName: string, password: string): Observable<any> {
+    return this.httpclient.post("http://localhost:3000/user/login", { userName, password })
+  }
 }
